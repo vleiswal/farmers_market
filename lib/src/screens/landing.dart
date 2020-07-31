f7f191af7e40bfc16b374cb3bb6f4ac64a0080cd
Vleis Walker <vleiswal@gmail.com>
2020-07-31 19:40:34 +0200

    40Finish

--- lib/src/screens/landing.dart
+++ lib/src/screens/landing.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
 
+import 'package:farmers_market/src/styles/base.dart';
+import 'package:farmers_market/src/styles/colors.dart';
+import 'package:farmers_market/src/styles/text.dart';
 import 'package:farmers_market/src/widgets/button.dart';
+import 'package:farmers_market/src/widgets/list_tile.dart';
+import 'package:farmers_market/src/widgets/sliver_scaffold.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,11 +13,15 @@ class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (Platform.isIOS) {
-      return CupertinoPageScaffold(
-        child: pageBody(context),
+      return AppSliverScaffold.cupertinoSliverScaffold(
+        navTitle: 'Upcoming Markets',
+        pageBody: Scaffold(body: pageBody(context)),
       );
     } else {
-      return Scaffold(body: pageBody(context));
+      return AppSliverScaffold.materialSliverScaffold(
+        navTitle: 'Upcoming Markets',
+        pageBody: pageBody(context),
+      );
     }
   }
 
@@ -20,10 +29,82 @@ class Landing extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        AppButton(
-          buttonText: 'Vendor Page',
-          buttonType: ButtonType.Straw,
-          onPressed: () => Navigator.pushNamed(context, '/vendor'),
+        Flexible(
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: -10.0,
+                child: Image.asset('assets/images/market.jpg'),
+              ),
+              Positioned(
+                bottom: 10.0,
+                right: 10.0,
+                child: GestureDetector(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.lightblue,
+                      borderRadius:
+                          BorderRadius.circular(BaseStyles.borderRadius),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Vendor Page',
+                          style: TextStyles.buttonTextLight),
+                    ),
+                  ),
+                  onTap: () => Navigator.of(context).pushNamed('/vendor'),
+                ),
+              ),
+            ],
+          ),
+          flex: 2,
+        ),
+        Flexible(
+          child: ListView(
+            children: <Widget>[
+              AppListTile(
+                title: 'Brits Boermark',
+                location: 'Aftreeoord',
+                month: 'Sept',
+                date: '4',
+                acceptingOrders: true,
+              ),
+              AppListTile(
+                  title: 'Groentemark',
+                  location: 'Faan se Plaas',
+                  month: 'Oct',
+                  date: '23'),
+              AppListTile(
+                title: 'Brits Boermark',
+                location: 'Aftreeoord',
+                month: 'Sept',
+                date: '4',
+                acceptingOrders: true,
+              ),
+              AppListTile(
+                title: 'Brits Boermark',
+                location: 'Aftreeoord',
+                month: 'Sept',
+                date: '4',
+                acceptingOrders: true,
+              ),
+              AppListTile(
+                title: 'Brits Boermark',
+                location: 'Aftreeoord',
+                month: 'Sept',
+                date: '4',
+                acceptingOrders: true,
+              ),
+              AppListTile(
+                title: 'Brits Boermark',
+                location: 'Aftreeoord',
+                month: 'Sept',
+                date: '4',
+                acceptingOrders: true,
+              ),
+            ],
+          ),
+          flex: 3,
         )
       ],
     );
---
+++ lib/src/widgets/list_tile.dart
@@ -0,0 +1,59 @@
+import 'package:farmers_market/src/styles/base.dart';
+import 'package:farmers_market/src/styles/colors.dart';
+import 'package:farmers_market/src/styles/text.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class AppListTile extends StatelessWidget {
+  final String month;
+  final String date;
+  final String title;
+  final String location;
+  final bool acceptingOrders;
+
+  AppListTile({
+    @required this.month,
+    @required this.date,
+    @required this.title,
+    @required this.location,
+    this.acceptingOrders = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: CircleAvatar(
+            backgroundColor: AppColors.lightblue,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 3.0),
+                  child: Text(month,
+                      style: TextStyle(color: Colors.white, fontSize: 10.0)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: Text(date, style: TextStyles.buttonTextLight),
+                ),
+              ],
+            ),
+          ),
+          title: Text(title, style: TextStyles.subtitle),
+          subtitle: Text(location),
+          trailing: (acceptingOrders)
+              ? Icon(FontAwesomeIcons.shoppingBasket, color: AppColors.darkblue)
+              : Text(' '),
+        ),
+        Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: BaseStyles.listFieldHorizontal),
+          child: Divider(
+            color: AppColors.lightgray,
+          ),
+        ),
+      ],
+    );
+  }
+}
